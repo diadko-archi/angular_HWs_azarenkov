4d37914525aae039841b9e37b72011a66852fdf7
ArturSlasher <archiblablablapicher@gmail.com>
2022-09-08 11:55:33 +0300

    hw3

---
+++ hw3/app.js
@@ -0,0 +1,10 @@
+let number1 = prompt("Введите первое число");
+let number2 = prompt("Введите второе число");
+let sum = +number1 + +number2;
+
+if (sum >= 20) {
+    alert("😁");
+}
+else {
+    alert("😢");
+}

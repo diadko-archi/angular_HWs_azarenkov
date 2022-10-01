ebb339883b3dadf580d09d1a26e8bfa42f2591cc
ArturSlasher <archiblablablapicher@gmail.com>
2022-10-01 19:06:15 +0300

    hw4 try2

--- hw4/app.js
+++ hw4/app.js
@@ -1,18 +1,12 @@
-var cells = document.getElementsByTagName("div");
+const cells = document.getElementsByTagName("div");
 let turn = 0;
 
 for(let i = 0; i < 9; i++){
     cells[i].onclick = function() {
-        turn = ++turn;
-        let color = cells[i].className;
-        console.log(cells[i], color)
-        if (color == "uncolored" && color != "yellow" && turn%2 != 0) {
-            cells[i].classList.remove("uncolored");
-            cells[i].classList.add("blue");
-        }
-        else if (color == "uncolored" && color != "blue") {
-            cells[i].classList.remove("uncolored");
-            cells[i].classList.add("yellow");
+        let color = turn%2 == 0 ? "blue" : "yellow";
+        if (cells[i].className != "blue" && cells[i].className != "yellow"){
+            cells[i].classList.toggle(color)
+            turn = ++turn;
         }
     }
 }
